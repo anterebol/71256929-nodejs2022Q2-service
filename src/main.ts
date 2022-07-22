af8ba907e070f41067cca3b43e42b8151958a28a
unknown <anterebol@gmail.com>
2022-07-22 13:42:09 +0300

    feat: add Dockerfile for app

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,6 @@ const PORT = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
+  console.log(`Server work at ${PORT}`);
 }
 bootstrap();
